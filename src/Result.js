f45f52cd4eb8773f933cfa497f0bc492a95bb123
Shubham Kapoor <shubham.kapoor01@gmail.com>
2022-03-09 21:25:57 +0530

    debug

--- src/Result.js
+++ src/Result.js
@@ -17,7 +17,7 @@ function Result(props) {
 
 	const showAllowed = async (userToCheckAllowed) => {
 		if (typeof window.ethereum !== 'undefined') {
-			console.log(props.roomId);
+			console.log(props.roomId, userToCheckAllowed);
 			const provider = new ethers.providers.Web3Provider(window.ethereum);
 			const contract = new ethers.Contract(lockAddress, Lock.abi, provider);
 			try {
