513ce1a7a9cbd226a6d69ae57dddb7680878b831
Shubham Kapoor <shubham.kapoor01@gmail.com>
2022-03-16 20:37:55 +0530

    backend

---
+++ server/server.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const app = express();
+const cors = require("cors");
+const { Server } = require("socket.io");
+const { pipeline } = require("serialport");
+var SerialPort = require("serialport");
+
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+	cors: {
+		origin: '*',
+		methods: ['GET', 'POST'],
+	},
+});
+
+const parsers = SerialPort.parsers;
+const parser = new parsers.Readline({
+	delimiter: '\r\n'
+});
+
+var port = new SerialPort("/dev/cu.usbmodem14401", {
+	baudRate: 9600,
+	dataBits: 8,
+	parity: 'none',
+	stopBits: 1,
+	flowControl: false
+});
+
+port.pipe(parser);
+
+// setTimeout(() => {
+// 	port.write("1");
+// }, 3000);
+
+io.on('connection', (socket) => {
+	socket.on('result', (data) => {
+		console.log(data);
+	})
+})
+
+const Port = process.env.PORT || 3001;
+server.listen(Port, () => console.log(`server is running on port ${Port}`));
